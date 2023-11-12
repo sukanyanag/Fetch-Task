b42312b8f8ed190bc3a006e350e669eff4369327
Sukanya Nag <60750067+sukanyanag@users.noreply.github.com>
2023-11-11 21:14:55 -0500

    Fixed null issue

--- app/src/main/java/com/example/fetchtask/Item.java
+++ app/src/main/java/com/example/fetchtask/Item.java
@@ -5,7 +5,7 @@ public class Item {
     private int listId;
     private String name;
 
-    // Constructor, getters, and setters
+    // Constructor, getters and setters
     public Item(int id, int listId, String name) {
         this.id = id;
         this.listId = listId;
@@ -16,11 +16,24 @@ public class Item {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public int getListId() {
         return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
     }
 
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
--- app/src/main/java/com/example/fetchtask/MainActivity.java
+++ app/src/main/java/com/example/fetchtask/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.NetworkError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ItemsAdapter adapter;
     private List<Item> itemList = new ArrayList<>();
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.RecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        progressBar = findViewById(R.id.progressBar);
+
         adapter = new ItemsAdapter(itemList);
         recyclerView.setAdapter(adapter);
 
@@ -39,54 +48,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchData() {
-
+        progressBar.setVisibility(View.VISIBLE);
         String url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
 
-        // Create a request queue
+        // Creating a request queue
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        // Request a JSON response from the provided URL
+        // Requesting a JSON response from the provided URL
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
                 response -> {
-                    try {
-                        // Parsing JSON array
-                        for (int i = 0; i < response.length(); i++) {
-                            JSONObject jsonObject = response.getJSONObject(i);
+                    // Processing in a background thread
+                    new Thread(() -> {
+                        try {
+                            // Clear existing data
+                            itemList.clear();
 
-                            // Extracting fields from JSON object
-                            int id = jsonObject.getInt("id");
-                            int listId = jsonObject.getInt("listId");
-                            String name = jsonObject.optString("name", "");
+                            // Parsing JSON array
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject jsonObject = response.getJSONObject(i);
 
-                            // Filter out items where "name" is blank or null
-                            if (!name.trim().isEmpty()) {
-                                // Add item to the list
+                                // Extracting fields from JSON object
+                                int id = jsonObject.getInt("id");
+                                int listId = jsonObject.getInt("listId");
+                                String name = jsonObject.optString("name", "");
+
+                                // Check for both actual null, the string "null", empty, and whitespace only
+                                if (name == null || "null".equals(name) || name.trim().isEmpty()) {
+                                    continue;
+                                }
+
+                                // Adding item to the list
                                 itemList.add(new Item(id, listId, name));
                             }
-                        }
 
-                        // Sort the itemList
-                        Collections.sort(itemList, (item1, item2) -> {
-                            // First by listId
-                            if (item1.getListId() != item2.getListId()) {
-                                return Integer.compare(item1.getListId(), item2.getListId());
+                            // Sorting the itemList
+                            Collections.sort(itemList, (item1, item2) -> {
+                                // First by listId
+                                if (item1.getListId() != item2.getListId()) {
+                                    return Integer.compare(item1.getListId(), item2.getListId());
+                                }
+                                // Then by name
+                                return item1.getName().compareToIgnoreCase(item2.getName());
+                            });
+
+                            for (Item item : itemList) {
+                                Log.d("MainActivity", "Filtered Item: " + item.getName());
                             }
-                            // Then by name
-                            return item1.getName().compareToIgnoreCase(item2.getName());
-                        });
 
-                        // Notify adapter about the new data
-                        adapter.notifyDataSetChanged();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        // Handle the JSON exception
-                    }
+                            // Update the UI on the main thread after processing
+                            runOnUiThread(() -> {
+                                adapter.notifyDataSetChanged();
+                                // Hide progress bar on successful fetch
+                                progressBar.setVisibility(View.GONE);
+                            });
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            runOnUiThread(() -> progressBar.setVisibility(View.GONE));
+                        }
+                    }).start();
                 },
                 error -> {
-                    // Handle the error
+                    // Handling the error
+                    String errorMessage = error instanceof NetworkError ? "Network error!" : "Unexpected error!";
+                    Snackbar.make(findViewById(R.id.main_layout), errorMessage, Snackbar.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
                 });
 
-        // Add the request to the RequestQueue
+        // Adding the request to the RequestQueue
         queue.add(jsonArrayRequest);
     }
 }
