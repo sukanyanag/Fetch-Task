106709b2deeaffbb5467107d886f2cb99d3fb6c0
Sukanya Nag <60750067+sukanyanag@users.noreply.github.com>
2023-11-12 20:29:38 -0500

    Documentation added

--- app/src/main/java/com/example/fetchtask/Item.java
+++ app/src/main/java/com/example/fetchtask/Item.java
@@ -1,16 +1,28 @@
 package com.example.fetchtask;
 
+/**
+ * Represents an item entity in the application.
+ * This class is used to model the data structure for items fetched.
+ */
 public class Item {
     private int id;
     private int listId;
     private String name;
 
-    // Constructor and getters
+    /**
+     * Constructor for Item.
+     *
+     * @param id     The unique identifier for the item.
+     * @param listId The identifier for the list to which this item belongs.
+     * @param name   The name of the item.
+     */
     public Item(int id, int listId, String name) {
         this.id = id;
         this.listId = listId;
         this.name = name;
     }
+
+    //Getter methods
 
     public int getId() {
         return id;
